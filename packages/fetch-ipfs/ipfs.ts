62b0f4cd4eeaabe83f33d69542c033be859d48e1
bluelovers <codelovers@users.sourceforge.net>
2020-02-27 19:43:56 +0800

    fix: avoid some api server limit

--- packages/fetch-ipfs/ipfs.ts
+++ packages/fetch-ipfs/ipfs.ts
@@ -15,7 +15,7 @@ export function refIPFS(cid: string, ipfs, timeout?: number)
 			{
 				if (ref.err)
 				{
-					return Bluebird.reject(ref.err)
+					return Promise.reject(ref.err)
 				}
 				else
 				{
@@ -30,6 +30,17 @@ export function catIPFS(cid: string, ipfs, timeout?: number)
 	timeout = timeout |= 0 || 60 * 1000;
 
 	return refIPFS(cid, ipfs)
+		.catch(Error, async (e: Error & {
+			response?: Response
+		}) => {
+			if (e.message && e.message.toLowerCase().includes('ipfs method not allowed'))
+			{
+				console.warn(String(e).replace(/\s+$/, ''), `\nurl: ${e.response.url}`, `\nwill ignore this error and trying fetch content`);
+				return;
+			}
+
+			return Promise.reject(e)
+		})
 		.then(async () => {
 			const chunks = [];
 			for await (const chunk of ipfs.cat(cid, {
