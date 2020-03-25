65c96b62da226c61ac06f8a1247aaeb6f54bd81a
bluelovers <codelovers@users.sourceforge.net>
2020-03-25 22:20:43 +0800

    export interface IMultiaddrToURLOptions

--- packages/multiaddr-to-url/index.d.ts
+++ packages/multiaddr-to-url/index.d.ts
@@ -1,4 +1,12 @@
-export declare function multiaddrToURL(multiaddr: string, opts?: {
+export interface IMultiaddrToURLOptions {
+    /**
+     * When /tcp is the last (terminating) protocol HTTP is assumed by default (implicit assumeHttp: true)
+     * this means produced URIs will start with http:// instead of tcp://
+     * passing { assumeHttp: false } disables this behavior
+     *
+     * @default true
+     */
     assumeHttp?: boolean;
-}): URL;
+}
+export declare function multiaddrToURL(multiaddr: string, opts?: IMultiaddrToURLOptions): URL;
 export default multiaddrToURL;
--- packages/multiaddr-to-url/index.ts
+++ packages/multiaddr-to-url/index.ts
@@ -1,8 +1,18 @@
 import multiaddrToUri from 'multiaddr-to-uri';
 
-export function multiaddrToURL(multiaddr: string, opts?: {
-	assumeHttp?: boolean,
-})
+export interface IMultiaddrToURLOptions
+{
+	/**
+	 * When /tcp is the last (terminating) protocol HTTP is assumed by default (implicit assumeHttp: true)
+	 * this means produced URIs will start with http:// instead of tcp://
+	 * passing { assumeHttp: false } disables this behavior
+	 *
+	 * @default true
+	 */
+	assumeHttp?: boolean;
+}
+
+export function multiaddrToURL(multiaddr: string, opts?: IMultiaddrToURLOptions)
 {
 	return new URL(multiaddrToUri(multiaddr, opts))
 }
