65926c8c7837b6920d02e2d9574b33cf3836ee27
bluelovers <codelovers@users.sourceforge.net>
2020-03-29 02:53:11 +0800

    feat: add ipfsBootstrapList

---
+++ packages/ipfs-server-list/bootstrap.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by user on 2020/3/29.
+ */
+/**
+ * js-ipfs
+ * @type {string[]}
+ */
+export declare const ipfsBootstrapJS: string[];
+/**
+ * ipfs-desktop
+ * @type {string[]}
+ */
+export declare const ipfsBootstrapDesktop: string[];
+export declare const ipfsBootstrapList: string[];
+export default ipfsBootstrapList;
