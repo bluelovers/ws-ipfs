8edf186b58fae316f282a37533b50b230ce229d7
bluelovers <codelovers@users.sourceforge.net>
2020-04-02 00:46:39 +0800

    fix: avoid bug in browser

--- packages/ipfs-http-client/core.js
+++ packages/ipfs-http-client/core.js
@@ -32,7 +32,8 @@ function getDefaultServerList(options = {}) {
         /**
          * https://github.com/ipfs/js-ipfs/blob/master/packages/ipfs-http-client/src/lib/core.js
          */
-        host: typeof window === 'undefined' ? void 0 : 'localhost',
+        host: typeof window === 'undefined' ? void 0 : '127.0.0.1',
+        protocol: typeof window === 'undefined' ? void 0 : 'http',
     } } = options;
     ipfsServerList.push({
         ...urlObject,
