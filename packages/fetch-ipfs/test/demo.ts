1d171c855d06868e21c1178169ff153f0ac998dc
bluelovers <codelovers@users.sourceforge.net>
2020-08-05 04:58:47 +0800

    .

--- packages/fetch-ipfs/test/demo.ts
+++ packages/fetch-ipfs/test/demo.ts
@@ -3,8 +3,26 @@
  */
 import { outputFile } from 'fs-extra';
 import fetchIPFS from 'fetch-ipfs';
+import { statSync } from 'fs';
+import { publishToIPFSAll } from '../lib/put/all';
+import { filterList } from 'ipfs-server-list';
+
+console.log(`fs.stat`, statSync('./temp/111.png').size)
 
 fetchIPFS(`QmdPAhQRxrDKqkGPvQzBvjYe3kU8kiEEAd2J6ETEamKAD9`)
-	.then(buf => outputFile('./temp/111.png', buf))
+	.then(async (buf) => {
+
+		console.log(`fetch ipfs cid`, buf.length, `QmdPAhQRxrDKqkGPvQzBvjYe3kU8kiEEAd2J6ETEamKAD9`)
+
+		await publishToIPFSAll(buf, [
+			...filterList('API'),
+		])
+			.then(result => {
+				console.log(`add to ipfs again`, result[0].value[0].size, result[0].value[0].cid.toString());
+			})
+		;
+
+		return outputFile('./temp/111.png', buf)
+	})
 	.catch(e => console.trace(e))
 ;
