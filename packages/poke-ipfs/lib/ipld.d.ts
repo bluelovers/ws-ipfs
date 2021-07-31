26efcab085209b01adb2c12c196683dec49a10c6
bluelovers <codelovers@users.sourceforge.net>
2021-07-31 08:28:31 +0800

    .

--- packages/poke-ipfs/lib/ipld.d.ts
+++ packages/poke-ipfs/lib/ipld.d.ts
@@ -1,10 +1,10 @@
 import { IPokeReturnBase, IPokeOptions } from './types';
 import { ICIDValue } from '@lazy-ipfs/detect-cid-lib/lib/types';
-export declare function pokeIPLD(cid: ICIDValue, options?: IPokeOptions): Promise<(Omit<IPokeReturnBase, "value"> & {
+export declare function pokeIPLD(cid: ICIDValue, options?: IPokeOptions): Promise<(Omit<IPokeReturnBase, "error"> & {
+    error: Error;
+}) | (Omit<IPokeReturnBase, "value"> & {
     value: true;
 }) | (Omit<IPokeReturnBase, "value"> & {
     value: false;
-}) | (Omit<IPokeReturnBase, "error"> & {
-    error: Error;
 })>;
 export default pokeIPLD;
